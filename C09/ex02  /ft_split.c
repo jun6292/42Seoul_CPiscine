1cbef9a4d39496e2559fe3e0b44f9df4bd252bbc
Wonjun Jo <wonjo@c7r2s5.42seoul.kr>
2022-05-01 22:58:53 +0900

    20220502

---
+++ C09/ex02  /ft_split.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wonjo <wonjo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/30 22:04:19 by wonjo             #+#    #+#             */
+/*   Updated: 2022/05/01 22:42:55 by wonjo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	is_sep(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	wc(char *str, char *charset)
+{
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 1;
+	while (str[i])
+	{
+		if (is_sep(str[i], charset) && !is_sep(str[i + 1], charset))
+			cnt++;
+		i++;
+	}
+	return (cnt);
+}
+
+char	*malloc_word(char *str, char *charset)
+{
+	char	*word;
+	int		i;
+	
+	i = 0;
+	while (str[i] && !is_sep(str[i], charset))
+		i++;
+	word = (char *)malloc(sizeof(char) * (i + 1));
+	while (str[i] && !is_sep(str[i], charset))
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**str_split(char *str, char *charset)
+{
+	char	**result;
+	int		i;
+	int		word_cnt;
+	int		word_arr;
+	
+	word_cnt = wc(str, charset);
+	result = (char **)malloc(sizeof(char *) * (word_cnt + 1));
+	if (!result)
+		return (NULL);
+	word_arr = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] && is_sep(str[i], charset))
+			i++;
+		if (str[i] && !is_sep(str[i], charset))
+		{
+			result[word_arr] = malloc_word(&str[i], charset);
+			word_arr++;
+		}
+		while (str[i] && !is_sep(str[i], charset))
+			i++;
+	}
+	result[word_arr] = 0;
+	return (result);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	printf("-------ex05-------\n\n");
+    char **ex05_split1;
+    char **ex05_split2;
+    char **ex05_split3;
+	char *ex05_str1 = "hello my name is hunpark!";
+	char *ex05_str2 = "good@day!cat#dog!vim12!";
+	char *ex05_str3 = "i1my2me3mine4you5your6you7yours";
+	ex05_split1 = ft_split(ex05_str1, " ");
+    ex05_split2 = ft_split(ex05_str2, "@!#");
+    ex05_split3 = ft_split(ex05_str3, "1234567");
+    printf("---test1---\n");
+	for (int j = 0; j < 5; j++)
+	{
+		printf("%s\n", ex05_split1[j]);
+	}
+    printf("\n\n");
+    printf("---test2---\n");
+    for (int j = 0; j < 5; j++)
+	{
+		printf("%s\n", ex05_split2[j]);
+	}
+    printf("\n\n");
+    printf("---test3---\n");
+    for (int j = 0; j < 8; j++)
+	{
+		printf("%s\n", ex05_split3[j]);
+	}
+    printf("\n\n");
+	free(ex05_split1);
+    free(ex05_split3);
+    free(ex05_split2);
+}
